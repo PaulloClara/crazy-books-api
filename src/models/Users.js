b4e8bb430a2ac7ad6f1df794c735874155268b9f
PaulloClara <paulloclara@gmail.com>
2019-08-13 00:32:31 -0300

    add campos para permitir login.

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,9 +3,26 @@ const mongoose = require('../database');
 const UsersSchema = mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    selected: false,
   },
 });
 
