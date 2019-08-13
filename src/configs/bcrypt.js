0d35b66ea6364ccabba08d2e2b7465f5e5774150
PaulloClara <paulloclara@gmail.com>
2019-08-13 00:31:26 -0300

    add bcrypt e jwt (config inicial)

---
+++ src/configs/bcrypt.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  hash(password) {
+    return bcrypt.hash(password, 12);
+  },
+  compare(password, hash) {
+    return bcrypt.compare(password, hash);
+  },
+};
---
+++ src/configs/jwt.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+
+const privateKey = process.env.PRIVATE_KEY;
+
+module.exports = {
+  sign(id){
+    return jwt.sign({ id }, privateKey, { expiresIn: 86400 });
+  },
+  verify(token) {
+    return jwt.verify(token, privateKey, (err, decoded) => (err ? false : decoded));
+  },
+};
