db032315f6acfd4e40bbbd8db6b6c3936b12d6b8
PaulloClara <paulloclara@gmail.com>
2019-09-13 10:50:56 -0300

    Att modules

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+
 mongoose.connect(process.env.MONGO_URL, {
   useCreateIndex: true,
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 mongoose.Promise = global.Promise;
 
